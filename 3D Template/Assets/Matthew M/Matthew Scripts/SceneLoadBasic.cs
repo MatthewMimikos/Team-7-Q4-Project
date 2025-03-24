93d88f5b1c1b4cf4f11ffbfbbeebe824fbc79da3
MUKILTEO\MatthewMimikos <1717212@mukilteo.wednet.edu>
2025-03-24 08:50:45 -0700

    git commit -m "Gitbash more like Gitbash"

--- 3D Template/Assets/Matthew M/Matthew Scripts/SceneLoadBasic.cs
+++ 3D Template/Assets/Matthew M/Matthew Scripts/SceneLoadBasic.cs
@@ -4,11 +4,24 @@ using UnityEngine.SceneManagement;
 public class SceneLoadBasic : MonoBehaviour
 {
 
+    public float WaitTime;
     public string scene;
+    public GameObject Image;
+
+    public void Awake()
+    {
+        Image.SetActive(false);
+    }
 
     public void LoadScene()
     {
         SceneManager.LoadScene(scene);
+    }
+
+    public void UniqueExit()
+    {
+        Invoke("LoadScene", WaitTime);
+        Image.SetActive(true);
     }
 
     public void End()
