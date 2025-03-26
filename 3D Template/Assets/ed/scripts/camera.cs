525e75248585d800951db3c7d3e7b18c9b392700
MUKILTEO\1717481 <1717481@mukilteo.wednet.edu>
2025-03-26 10:26:36 -0700

    top 10 jumpscare

--- 3D Template/Assets/ed/scripts/camera.cs
+++ 3D Template/Assets/ed/scripts/camera.cs
@@ -4,11 +4,16 @@ public class camera : MonoBehaviour
 {
     public bool can_see_player = false;
     private GameObject player;
+    public GameObject detection_visual;
+    private ui ui;
     public GameObject actual_camera;
+
+    public GameObject my_detection_visual;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GameObject.Find("Player");
+        ui = FindFirstObjectByType<ui>();
     }
 
     // Update is called once per frame
@@ -21,8 +26,14 @@ public class camera : MonoBehaviour
                 Debug.Log(hitInfo.transform.gameObject.name);
             }
         }
+        if (my_detection_visual != null)
+        {
+            if (my_detection_visual.transform.rotation.z != Vector3.Angle(actual_camera.transform.position, player.transform.position))
+            {
+                my_detection_visual.transform.Rotate(0, 0, Vector3.Angle(actual_camera.transform.position, player.transform.position));
+            }
+        }
     }
-
     private void OnDrawGizmos()
     {
         if (can_see_player)
@@ -31,11 +42,14 @@ public class camera : MonoBehaviour
         }
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             can_see_player = true;
+            GameObject new_detection = Instantiate(detection_visual, ui.transform.position, Quaternion.identity);
+            new_detection.transform.SetParent(ui.crosshair.transform);
+            my_detection_visual = new_detection.gameObject;
         }
     }
     private void OnTriggerExit(Collider other)
