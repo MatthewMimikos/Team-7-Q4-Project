982818e7ee3f263cecba89bcd8d7f9eec3cf7cdc
MUKILTEO\1717483 <1717483@mukilteo.wednet.edu>
2025-03-26 09:42:10 -0700

    RAH

--- 3D Template/Assets/Oscar Stuff/Scripts/Mask Switching.cs
+++ 3D Template/Assets/Oscar Stuff/Scripts/Mask Switching.cs
@@ -7,13 +7,15 @@ public class MaskSwitching : MonoBehaviour
     public RawImage Mask1;
     public RawImage Mask2;
     public RawImage Mask3;
-    public bool maskActive;
+    public bool mask1Active;
 
     public KeyCode useMask;
+    public KeyCode noMask;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Mask2.enabled = false; Mask3.enabled = false;
+        mask1Active = false;
     }
 
     // Update is called once per frame
@@ -21,57 +23,45 @@ public class MaskSwitching : MonoBehaviour
     {
         if (Input.GetKeyDown("1"))
         {
-            Mask1.enabled = true;
-            Mask2.enabled = false;
-            Mask3.enabled = false;
+            Equip1();
         }
         if (Input.GetKeyDown("2"))
         {
-            Mask1.enabled = false;
-            Mask2.enabled = true;
-            Mask3.enabled = false;
+            Equip2();
         }
         if (Input.GetKeyDown("3"))
         {
-            Mask1.enabled = false;
-            Mask2.enabled = false;
-            Mask3.enabled = true;
-        }
-        if (Mask1.enabled == true)
-        {
-            Equip1();
-        }
-        else if (Mask2.enabled == true)
-        {
-            Equip2();
-        }
-        else if (Mask3.enabled == true)
-        {
             Equip3();
+        }
+
+        if (Input.GetKeyDown(useMask) && Mask1.enabled == true && mask1Active == false)
+        {
+            GetComponent<PlayerMovement>().moveSpeed = 50;
+            mask1Active = true;
+        }
+        if (Input.GetKeyDown(noMask) && Mask1.enabled && mask1Active == true)
+        {
+            GetComponent<PlayerMovement>().moveSpeed = 7;
+            mask1Active = false;
         }
     }
 
     void Equip1()
     {
-
-
-        if (Input.GetKeyDown(useMask) && Mask1.enabled == true)
-        {
-            GetComponent<PlayerMovement>().moveSpeed = 50;
-            maskActive = true;
-        }
-        if (Input.GetKeyDown(useMask) && maskActive == true)
-        {
-            GetComponent<PlayerMovement>().moveSpeed = 8;
-            maskActive = false;
-        }
+        Mask1.enabled = true;
+        Mask2.enabled = false;
+        Mask3.enabled = false;
     }
     void Equip2()
     {
-
+        Mask1.enabled = false;
+        Mask2.enabled = true;
+        Mask3.enabled = false;
     }   
     void Equip3()
     {
-
+        Mask1.enabled = false;
+        Mask2.enabled = false;
+        Mask3.enabled = true;
     }
 }
